b4223d0c15dc13faff9a700265890a4be1c2dba0
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2022-11-12 21:59:49 -0500

    call the function of api

--- homepage.js
+++ homepage.js
@@ -1,4 +1,15 @@
+
+
+
+
+
+
+
+
+
+
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -14,6 +25,15 @@ var formSubmitHandler = function(event) {
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
+  }
+};
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+    // clear old content
+    repoContainerEl.textContent = "";
   }
 };
 var getUserRepos = function(user) {
@@ -36,6 +56,21 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert("Unable to connect to GitHub");
     });
+};
+var getFeaturedRepos = function(language) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
 };
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
@@ -73,5 +108,6 @@ var displayRepos = function(repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 };
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
